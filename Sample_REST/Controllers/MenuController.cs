22354c82e79d24377dc58d7f0aa32ca5cf6b1152
Pratiksha <Pratiksha.Gambhire@emtecinc.com>
2022-03-24 13:56:42 +0530

    'created-sample-REST-using-.Net'

---
+++ Sample_REST/Controllers/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace Sample_REST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Here is your menu card!!");
+        }
+
+        [HttpPost]
+        public IActionResult Post(JObject payload)
+        {
+            return Ok(payload);
+        }
+    }
+}
+
